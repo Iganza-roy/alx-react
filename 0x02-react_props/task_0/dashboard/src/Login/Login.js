081563ab2fc29f5a74d91448a31740f6f2c38b1f
Iganza-roy <iganzaroy55@gmail.com>
2024-08-26 23:16:52 +0300

    Add task 0

---
+++ 0x02-react_props/task_0/dashboard/src/Login/Login.js
@@ -0,0 +1,12 @@
+const Login = () => {
+  <>
+    <p>Login to access the full dashboard</p>
+    <label htmlFor='email'>Email: </label>
+    <input type='email' id='email' />
+    <label htmlFor='password'> Password: </label>
+    <input type='password' id='password' />
+    <button>OK</button>
+  </>;
+};
+
+export default Login;
