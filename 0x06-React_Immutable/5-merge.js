dd6f138f25291d7d1974790d665440ba28d60619
Iganza-roy <iganzaroy55@gmail.com>
2024-09-11 19:55:02 +0300

    Refactor code to use 'Map' and 'list' from Immutable for creating an immutable object and accessing nested values

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,13 @@
+import { list, Map } from 'Immutable';
+
+export function concatElements(page1, page2) {
+  return list(page1).concat(list(page2));
+}
+
+export function mergeElements(page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+
+  const merge = map1.merge(map2);
+  return merge.toList();
+}
