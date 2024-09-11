200550e5664ba7db40cbb85f3e7a6b37ca83055a
Iganza-roy <iganzaroy55@gmail.com>
2024-09-11 20:09:29 +0300

    Refactor code to use 'immutable' instead of 'Immutable' for importing List and Map in 5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,12 +1,5 @@
-import { List, Map } from 'Immutable';
+import { List, Map } from 'immutable';
 
-export function concatElements(page1, page2) {
-  return List(page1).concat(List(page2));
-}
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
-export function mergeElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-
-  const merge = map1.merge(map2);
-}
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
