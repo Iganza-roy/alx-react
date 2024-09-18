40e3eb1b2dd7601a5f1a5ebc28241ebd2ddb357c
Iganza-roy <iganzaroy55@gmail.com>
2024-09-18 14:19:27 +0300

    Refactor code to update Jest version and fix test file import paths

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,6 +1,6 @@
 const notifications_data = require('../../notifications.json');
 
-export default function getAllNotificationsByUser(userId) {
+export function getAllNotificationsByUser(userId) {
   return notifications_data
     .filter((notification) => notification.author.id === userId)
     .map((notification) => notification.context);
