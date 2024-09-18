b9a771be8c9a89354ff29388b426bd32972825e2
Iganza-roy <iganzaroy55@gmail.com>
2024-09-18 20:42:30 +0300

    Add task 3

---
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/schema/Notifications.js
@@ -0,0 +1,23 @@
+import { normalize, schema } from 'normalizr';
+const notifications_data = require('../../notifications.json');
+
+const user = new schema.Entity('users');
+const message = new schema.Entity(
+  'message',
+  {},
+  {
+    idAttribute: 'guid',
+  }
+);
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+export const normalizedData = normalize(notifications_data, [notification]);
+
+export function getAllNotificationsByUser(userId) {
+  return notifications_data
+    .filter((notification) => notification.author.id === userId)
+    .map((notification) => notification.context);
+}
